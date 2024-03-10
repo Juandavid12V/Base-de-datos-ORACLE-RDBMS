8bc2ce8cc698d4cd79750f486d59ec1c240a573a
Juan Vega <juan.vega01@uceva.edu.co>
2024-03-09 20:20:33 -0500

    Segundo Commit

--- OracleRDBMS.sql
+++ OracleRDBMS.sql
@@ -1,17 +1,25 @@
+--Presentado por: Juan David Vega 230201021
+--Crear usuario
+
+CREATE USER DAVIDBD IDENTIFIED BY "12345";  --crear usuario con su respectiva contraseña
+
+CONNECT DAVIDBD/12345@localhost:1521/freepdb1;
+
+
 --crear tablas
 CREATE TABLE review (
     id VARCHAR2(50),
-    updated_at TIMESTAMP,
-    created_at TIMESTAMP,
+    updated_at DATE,
+    created_at DATE,
     user_id VARCHAR2(50),
     place_id VARCHAR2(50),
     text VARCHAR2(255)
 );
 
-CREATE TABLE "User" (
+CREATE TABLE User_RDB (
     id VARCHAR2(50),
-    updated_at TIMESTAMP,
-    created_at TIMESTAMP,
+    updated_at DATE,
+    created_at DATE,
     email VARCHAR2(255),
     password VARCHAR2(255),
     first_name VARCHAR2(100),
@@ -20,8 +28,8 @@ CREATE TABLE "User" (
 
 CREATE TABLE Amenity (
     id VARCHAR2(50),
-    updated_at TIMESTAMP,
-    created_at TIMESTAMP,
+    updated_at DATE,
+    created_at DATE,
     name VARCHAR2(255)
 );
 
@@ -32,22 +40,22 @@ CREATE TABLE PlaceAmenity (
 
 CREATE TABLE State (
     id VARCHAR2(50),
-    updated_at TIMESTAMP,
-    created_at TIMESTAMP
+    updated_at DATE,
+    created_at DATE
 );
 
 CREATE TABLE City (
     id VARCHAR2(50),
-    updated_at TIMESTAMP,
-    created_at TIMESTAMP,
+    updated_at DATE,
+    created_at DATE,
     state_id VARCHAR2(50),
     name VARCHAR2(255)
 );
 
 CREATE TABLE Place (
     id VARCHAR2(50),
-    updated_at TIMESTAMP,
-    created_at TIMESTAMP,
+    updated_at DATE,
+    created_at DATE,
     user_id VARCHAR2(50),
     name VARCHAR2(255),
     city_id VARCHAR2(50),
@@ -59,35 +67,48 @@ CREATE TABLE Place (
     latitude FLOAT,
     longitude FLOAT
 );
---crear restricciones
-
---REVIEW
-ALTER TABLE review
-ADD CONSTRAINT pk_review PRIMARY KEY (id);
-ALTER TABLE review
-ADD CONSTRAINT fk_review_user FOREIGN KEY (user_id) REFERENCES "User" (id);
-ALTER TABLE review
-ADD CONSTRAINT fk_review_place FOREIGN KEY (place_id) REFERENCES Place (id);
-
+--Crear Restricciones:
+--Llaves Primarias
 --USER
-ALTER TABLE "User"
+ALTER TABLE User_RDB
 ADD CONSTRAINT pk_user PRIMARY KEY (id);
 
 --PLACE
 ALTER TABLE Place
 ADD CONSTRAINT pk_place PRIMARY KEY (id);
-ALTER TABLE Place
-ADD CONSTRAINT fk_place_user FOREIGN KEY (user_id) REFERENCES "User" (id);
 
-ALTER TABLE Place
-ADD CONSTRAINT fk_place_city FOREIGN KEY (city_id) REFERENCES City (id);
-
+--CITY
+ALTER TABLE City
+ADD CONSTRAINT pk_city PRIMARY KEY (id);
 
 --AMENITY
 ALTER TABLE Amenity
 ADD CONSTRAINT pk_amenity PRIMARY KEY (id);
 
---PLACEAMINITY
+--STATE
+ALTER TABLE State
+ADD CONSTRAINT pk_state PRIMARY KEY (id);
+
+--REVIEW
+ALTER TABLE review
+ADD CONSTRAINT pk_review PRIMARY KEY (id);
+
+
+--Llaves Foraneas
+ALTER TABLE Place
+ADD CONSTRAINT fk_place_user FOREIGN KEY (user_id) REFERENCES User_RDB (id);
+ALTER TABLE Place
+ADD CONSTRAINT fk_place_city FOREIGN KEY (city_id) REFERENCES City (id);
+
+
+
+ALTER TABLE review
+ADD CONSTRAINT fk_review_user FOREIGN KEY (user_id) REFERENCES User_RDB (id);
+ALTER TABLE review
+ADD CONSTRAINT fk_review_place FOREIGN KEY (place_id) REFERENCES Place (id);
+
+
+
 ALTER TABLE PlaceAmenity
 ADD CONSTRAINT fk_placeamenity_amenity FOREIGN KEY (amenity_id) REFERENCES Amenity (id);
 
@@ -95,14 +116,7 @@ ALTER TABLE PlaceAmenity
 ADD CONSTRAINT fk_placeamenity_place FOREIGN KEY (place_id) REFERENCES Place (id);
 
 
---STATE
-ALTER TABLE State
-ADD CONSTRAINT pk_state PRIMARY KEY (id);
-
---CITY
-ALTER TABLE City
-ADD CONSTRAINT pk_city PRIMARY KEY (id);
-
 ALTER TABLE City
 ADD CONSTRAINT fk_city_state FOREIGN KEY (state_id) REFERENCES State (id);
+
 
